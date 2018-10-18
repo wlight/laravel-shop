49a782038447d1615c9026e110e86edc18adcefc
wlight <wormside@gmail.com>
2018-10-18 02:27:37 +0000

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://www.weibo.com/u/2187879951?is_all=1">枫林断梦</a> 编码设计 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="pull-right"><a href="mailto:yiwuhui@live.com">联系我</a></p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- CSRF Token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title', 'Laravel shop') - Laravel 商城</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+        </div>
+        @include('layouts._footer')
+    </div>
+    {{-- Js 脚本 --}}
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
