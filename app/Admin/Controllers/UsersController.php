586e1505dbf49a7bf2b1ad139e45d67c46598017
wlight <wormside@gmail.com>
2018-10-31 11:29:20 +0000

    商品模

--- app/Admin/Controllers/UsersController.php
+++ app/Admin/Controllers/UsersController.php
@@ -5,12 +5,10 @@ namespace App\Admin\Controllers;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\HasResourceActions;
-use Encore\Admin\Facades\Admin;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
-use function foo\func;
 
 class UsersController extends Controller
 {
@@ -25,7 +23,7 @@ class UsersController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->header('Index')
+            ->header('用户列表')
             ->description('description')
             ->body($this->grid());
     }
@@ -46,36 +44,65 @@ class UsersController extends Controller
     }
 
     /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
      * Make a grid builder.
      *
      * @return Grid
      */
     protected function grid()
     {
-        // 根据回调函数，在页面上用表格的形式来展示用户记录
-        return Admin::grid(User::class, function (Grid $grid){
-            $grid->id('ID')->sortable();
+        $grid = new Grid(new User);
 
-            $grid->name('用户名');
+        $grid->id('Id')->sortable();
+        $grid->name('用户名');
+        $grid->email('邮箱');
+        $grid->email_verified('已验证邮箱')->dispaly(function ($value){
+            return $value ? "是" : "否";
+        });
 
-            $grid->email('邮箱');
-            $grid->email_verified('已验证邮箱')->display(function ($value){
-                return $value ? '是' : '否';
-            });
-            $grid->created_at('注册时间');
+        $grid->created_at('注册时间');
+        $grid->disableCreateButton();
+        $grid->actions(function ($actions){
+            $actions->disableView();
+            $actions->disableDelete();
+            $actions->disableEdit();
+        });
 
-            $grid->disableCreateButton();
-            $grid->actions(function ($actions){
-                $actions->disableView();
-                $actions->disableDelete();
-                $actions->disableEdit();
-            });
-            $grid->tools(function ($tools){
-                $tools->batch(function ($batch){
-                    $batch->disableDelete();
-                });
+        $grid->tools(function ($tools){
+            $tools->batch(function ($batch){
+                $batch->disableDelete();
             });
         });
+
+        return $grid;
     }
 
     /**
