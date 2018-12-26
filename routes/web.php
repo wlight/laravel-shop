04d96c275c5b4e0427c7b3acb0cff535ce07c54a
wlight <wormside@gmail.com>
2018-12-26 08:52:00 +0000

    用户界面 - 申请退款

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
         Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
+        Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
         Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
         Route::get('apyment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     });
